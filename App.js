d25703e3591cc23ee0be6aeb5f5b11333fb358f2
Tim Knapp <timknapp12@gmail.com>
2020-10-21 16:52:38 -0600

    add main colors

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// green = 43cc59
+// blue = 1982dd
+
 export default function App() {
   return (
     <View style={styles.container}>
