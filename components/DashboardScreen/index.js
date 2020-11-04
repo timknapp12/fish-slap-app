85f76fc482b3f04dea53204a168e216401235c22
Tim Knapp <timknapp12@gmail.com>
2020-11-03 17:29:50 -0700

    create dashboard screen

---
+++ components/DashboardScreen/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import firebase from "firebase";
+
+const DashboardScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>DashboardScreen</Text>
+      <Button title="Sign out" onPress={() => firebase.auth().signOut()} />
+    </View>
+  );
+};
+export default DashboardScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
