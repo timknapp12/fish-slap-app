7c6e073c5709a6a26949fa6560b0587a78e76ae5
Tim Knapp <timknapp12@gmail.com>
2020-11-12 17:21:57 -0700

    add router

---
+++ router.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./components/LoginScreen";
+import LoadingScreen from "./components/LoadingScreen";
+import DashboardScreen from "./components/DashboardScreen";
+
+const Login = createStackNavigator();
+
+export const LoginStack = () => {
+  return (
+    <Login.Navigator>
+      <Login.Screen name="LoadingScreen" component={LoadingScreen} />
+      <Login.Screen name="LoginScreen" component={LoginScreen} />
+      <Login.Screen name="DashboardScreen" component={DashboardScreen} />
+    </Login.Navigator>
+  );
+};
