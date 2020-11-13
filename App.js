6306d006b0e8672aed0b3da0616aa458c4cb414e
Tim Knapp <timknapp12@gmail.com>
2020-11-12 17:22:31 -0700

    restructure navigation stacks

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
+// import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginStack } from "./router";
 
 import LoginScreen from "./components/LoginScreen";
 import DashboardScreen from "./components/DashboardScreen";
@@ -10,18 +13,29 @@ import * as firebase from "firebase";
 import { firebaseConfig } from "./config";
 firebase.initializeApp(firebaseConfig);
 
+const Stack = createStackNavigator();
+
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <LoginStack />
+      {/* <Stack.Navigator>
+        <Stack.Screen name="Loading" component={LoadingScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+      </Stack.Navigator> */}
+    </NavigationContainer>
+  );
 };
 export default App;
 
-const AppSwitchNavigator = createSwitchNavigator({
-  LoadingScreen: LoadingScreen,
-  LoginScreen: LoginScreen,
-  DashboardScreen: DashboardScreen,
-});
+// const AppSwitchNavigator = createSwitchNavigator({
+//   LoadingScreen: LoadingScreen,
+//   LoginScreen: LoginScreen,
+//   DashboardScreen: DashboardScreen,
+// });
 
-const AppNavigator = createAppContainer(AppSwitchNavigator);
+// const AppNavigator = createAppContainer(AppSwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
