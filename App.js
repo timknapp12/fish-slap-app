01cbc2f45230a74d1637faaa59593c4d3e32301b
Tim Knapp <timknapp12@gmail.com>
2020-11-13 15:27:53 -0700

    add context rovider

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 // import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { LoginStack } from "./router";
+import AppContext from "./utils/AppContext";
 
 import LoginScreen from "./components/LoginScreen";
 import DashboardScreen from "./components/DashboardScreen";
@@ -16,15 +17,18 @@ firebase.initializeApp(firebaseConfig);
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [loadingLogin, setLoadingLogin] = useState(false);
   return (
-    <NavigationContainer>
-      <LoginStack />
-      {/* <Stack.Navigator>
+    <AppContext.Provider value={{ loadingLogin, setLoadingLogin }}>
+      <NavigationContainer>
+        <LoginStack />
+        {/* <Stack.Navigator>
         <Stack.Screen name="Loading" component={LoadingScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
       </Stack.Navigator> */}
-    </NavigationContainer>
+      </NavigationContainer>
+    </AppContext.Provider>
   );
 };
 export default App;
