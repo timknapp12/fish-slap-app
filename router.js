a3e54bd05b925c232c2160380a311a60367ac55e
Tim Knapp <timknapp12@gmail.com>
2020-11-13 18:33:08 -0700

    add icons to tabs

--- router.js
+++ router.js
@@ -1,9 +1,44 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
+import Ionicons from "react-native-vector-icons/MaterialCommunityIcons";
 import LoginScreen from "./components/LoginScreen";
 import LoadingScreen from "./components/LoadingScreen";
 import DashboardScreen from "./components/DashboardScreen";
+import ProfileScreen from "./components/Profile/ProfileScreen";
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "DashboardScreen") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "account" : "account-outline";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen
+        name="DashboardScreen"
+        component={DashboardScreen}
+        options={{ title: "Home" }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ title: "Profile" }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Login = createStackNavigator();
 
@@ -15,6 +50,7 @@ export const LoginStack = () => {
         name="LoginScreen"
         component={LoginScreen}
         options={{
+          title: "Please Login",
           gestureEnabled: false,
           headerLeft: () => {
             return null;
@@ -22,9 +58,10 @@ export const LoginStack = () => {
         }}
       />
       <Login.Screen
-        name="DashboardScreen"
-        component={DashboardScreen}
+        name="Tabs"
+        component={Tabs}
         options={{
+          headerTitle: "Fish Slap App",
           gestureEnabled: false,
           headerLeft: () => {
             return null;
