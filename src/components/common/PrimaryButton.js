d840d9a3f1c5713e54a11bc26f178d8a05f644d1
Tim Knapp <timknapp12@gmail.com>
2020-11-18 15:56:13 -0700

    add styled componetns to Primary button

--- src/components/common/PrimaryButton.js
+++ src/components/common/PrimaryButton.js
@@ -2,22 +2,32 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { blue, green } from "../../styles/colors";
 import { LinearGradient } from "expo-linear-gradient";
+import styled from "styled-components/native";
 
 const PrimaryButton = ({ children = "Button", ...props }) => (
   <View style={styles.button}>
     <LinearGradient colors={[blue, green]} start={{ x: 0.49, y: 0.2 }}>
-      <TouchableOpacity {...props}>
+      <Container style={{ height: 24 }} {...props}>
         <Text style={styles.text}>{children}</Text>
-      </TouchableOpacity>
+      </Container>
     </LinearGradient>
   </View>
 );
 
 export { PrimaryButton };
 
+const Container = styled.TouchableOpacity`
+  width: 100%;
+  height: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const styles = StyleSheet.create({
   button: {
     width: "100%",
+    // height: 24,
   },
   linearGradient: {
     flex: 1,
@@ -27,6 +37,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#ffffff",
-    textAlign: "center",
+    fontWeight: "700",
+    letterSpacing: 1.2,
   },
 });
