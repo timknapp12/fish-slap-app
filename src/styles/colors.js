1c51a4190cf6c135d475dd2d9f29182ccb6afd84
Tim Knapp <timknapp12@gmail.com>
2020-11-19 18:56:30 -0700

    add colors to syles

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -2,7 +2,7 @@ export const green = "#43cc59";
 
 export const blue = "#1982dd";
 
-export const pink = "#f55190";
+export const pink = "#ff00ff";
 
 export const lightPink = "#f5b8cf";
 
@@ -10,4 +10,6 @@ export const lightBlue = "#6699CC";
 
 export const black = "#11141c";
 
-export const darkBlue = "#111d42";
+export const darkBlue = "#1B1464";
+
+export const white = "#fcfcfc";
