5491ad523c125ec38ff5acf4439e2dc6f6f37101
Tim Knapp <timknapp12@gmail.com>
2020-11-30 17:30:48 -0700

    create four themes

---
+++ src/styles/themes.js
@@ -0,0 +1,35 @@
+import {
+  green,
+  blue,
+  pink,
+  lighPink,
+  lightBlue,
+  black,
+  darkBlue,
+  white,
+  yellow,
+} from "./colors";
+
+export const galaxyTheme = {
+  color: white,
+  linearGradientOne: pink,
+  linearGradientTwo: darkBlue,
+};
+
+export const midnightTheme = {
+  color: white,
+  linearGradientOne: darkBlue,
+  linearGradientTwo: black,
+};
+
+export const lightTheme = {
+  color: black,
+  linearGradientOne: white,
+  linearGradientTwo: white,
+};
+
+export const sunriseTheme = {
+  color: black,
+  linearGradientOne: white,
+  linearGradientTwo: yellow,
+};
