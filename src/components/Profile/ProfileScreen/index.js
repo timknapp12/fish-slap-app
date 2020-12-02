3dbeeb4e6150f4fcb4411d8aa57ae862dbc2382f
Tim Knapp <timknapp12@gmail.com>
2020-12-01 18:29:27 -0700

    add logic for selecting theme in profile screen

--- src/components/Profile/ProfileScreen/index.js
+++ src/components/Profile/ProfileScreen/index.js
@@ -1,11 +1,38 @@
-import React from "react";
-import { Text } from "react-native";
-import { ScreenContainer } from "../../common";
+import React, { useContext } from "react";
+import { ScreenContainer, MainText, GeneralContainer } from "../../common";
+import AppContext from "../../../utils/AppContext";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import {
+  lightTheme,
+  midnightTheme,
+  sunriseTheme,
+  galaxyTheme,
+} from "../../../styles/themes";
+
+const themes = [
+  { id: 1, name: lightTheme, displayName: "Light Theme" },
+  { id: 2, name: midnightTheme, displayName: "Midnight Theme" },
+  { id: 3, name: sunriseTheme, displayName: "Sunrise Theme" },
+  { id: 4, name: galaxyTheme, displayName: "Galaxy Theme" },
+];
 
 const ProfileScreen = () => {
+  const { setTheme } = useContext(AppContext);
   return (
     <ScreenContainer>
-      <Text style={{ color: "white" }}>Profile Screen</Text>
+      <MainText>Profile Screen</MainText>
+      <GeneralContainer padding={16}>
+        {themes.map((item) => (
+          <TouchableOpacity
+            key={item.id}
+            onPress={() => {
+              setTheme(item.name);
+            }}
+          >
+            <MainText>{item.displayName}</MainText>
+          </TouchableOpacity>
+        ))}
+      </GeneralContainer>
     </ScreenContainer>
   );
 };
