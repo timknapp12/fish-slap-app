a767ca33a545a0d1d19ec76afbe32a3ee679450a
Tim Knapp <timknapp12@gmail.com>
2020-12-03 10:58:56 -0700

    add image picker to profile screen

--- src/components/Profile/ProfileScreen/index.js
+++ src/components/Profile/ProfileScreen/index.js
@@ -1,26 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { StyleSheet, Image } from "react-native";
 import { ScreenContainer, MainText, GeneralContainer } from "../../common";
 import AppContext from "../../../utils/AppContext";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import {
-  lightTheme,
-  midnightTheme,
-  sunriseTheme,
-  galaxyTheme,
-} from "../../../styles/themes";
-
-const themes = [
-  { id: 1, name: lightTheme, displayName: "Light Theme" },
-  { id: 2, name: midnightTheme, displayName: "Midnight Theme" },
-  { id: 3, name: sunriseTheme, displayName: "Sunrise Theme" },
-  { id: 4, name: galaxyTheme, displayName: "Galaxy Theme" },
-];
+import * as ImagePicker from "expo-image-picker";
+import { themes } from "./data";
 
 const ProfileScreen = () => {
   const { setTheme } = useContext(AppContext);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+  if (selectedImage !== null) {
+    return (
+      <GeneralContainer>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </GeneralContainer>
+    );
+  }
   return (
     <ScreenContainer>
       <MainText>Profile Screen</MainText>
+      <GeneralContainer>
+        <TouchableOpacity onPress={openImagePickerAsync}>
+          <MainText>Upoad image</MainText>
+        </TouchableOpacity>
+      </GeneralContainer>
+
       <GeneralContainer padding={16}>
         {themes.map((item) => (
           <TouchableOpacity
@@ -38,3 +61,12 @@ const ProfileScreen = () => {
 };
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  /* Other styles hidden to keep the example brief... */
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  },
+});
