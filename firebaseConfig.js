f9bf4a390e29dfdb8f89777c4bc30ba508caf22f
Tim Knapp <timknapp12@gmail.com>
2020-12-28 14:38:53 -0700

    update config and api key

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCicvIArbPezm8tB9grTS-mAaS0qSuxwbQ",
+  authDomain: "fish-slap-app.firebaseapp.com",
+  databaseURL: "https://fish-slap-app.firebaseio.com",
+  projectId: "fish-slap-app",
+  storageBucket: "gs://fish-slap-app.appspot.com",
+  messagingSenderId: "378539798943",
+  appId: "1:378539798943:web:3fce6e2e8655c0cf",
+};
+
+export const androidClientId =
+  "378539798943-gb6ks3bu5ac5pranmi31fup5291l6ses.apps.googleusercontent.com";
+
+export const iosClientId =
+  "378539798943-j8jnl0lcob2e28odkncqmdbd9dph5u5g.apps.googleusercontent.com";
+
+export const faceBookAppId = "131989751762089";
