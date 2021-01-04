b094ffca858a9c730487922425bac7479192ac8b
Tim Knapp <timknapp12@gmail.com>
2021-01-04 16:46:07 -0700

    delete unneeded file

--- src/components/Profile/colorThemes.js
+++
@@ -1,8 +0,0 @@
-import {
-  lightTheme,
-  midnightTheme,
-  sunriseTheme,
-  galaxyTheme,
-} from "../../styles/themes";
-
-export const themes = [lightTheme, midnightTheme, sunriseTheme, galaxyTheme];
