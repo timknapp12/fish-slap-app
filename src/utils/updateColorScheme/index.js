54904d9e4aa97910ccefbab03a9bf54188e3f524
Tim Knapp <timknapp12@gmail.com>
2021-01-05 01:11:53 -0700

    finish updateColorScheme function

--- src/utils/updateColorScheme/index.js
+++ src/utils/updateColorScheme/index.js
@@ -1,33 +1,54 @@
-import React from "react";
-import { useColorScheme } from "react-native";
 import * as firebase from "firebase";
+import {
+  lightTheme,
+  midnightTheme,
+  sunriseTheme,
+  galaxyTheme,
+} from "../../styles/themes";
 
-const updateColorScheme = () => {
-  const colorScheme = useColorScheme() || "light";
-  const user = firebase?.auth()?.currentUser;
-  //   console.log("user", user);
+const updateColorScheme = (colorScheme, user, setUpdateToFirebasePending) => {
+  let newTheme;
+  if (!user.theme.isSyncedToDevice) {
+    newTheme = matchTheme(user.theme.defaultTheme);
+    return saveThemeToDB(user, newTheme, setUpdateToFirebasePending);
+  }
+  if (user.theme.isSyncedToDevice) {
+    newTheme =
+      colorScheme === "light"
+        ? matchTheme(user.theme.lightTheme)
+        : matchTheme(user.theme.darkTheme);
+    return saveThemeToDB(user, newTheme, setUpdateToFirebasePending);
+  }
+};
+export default updateColorScheme;
 
-  if (user) {
-    const db = firebase.firestore();
-    // const theme = db?.collection("users")?.doc("eke6QmalcWbHAIyTv9m4lOkpBDx2");
-
-    // console.log("theme", theme);
-    const docRef = db.collection("users").doc(user.uid);
-
-    docRef
-      .get()
-      .then(function (doc) {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-        } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-        }
-      })
-      .catch(function (error) {
-        console.log("Error getting document:", error);
-      });
+const matchTheme = (themeName) => {
+  switch (themeName) {
+    case "lightTheme":
+      return lightTheme;
+      break;
+    case "midnightTheme":
+      return midnightTheme;
+      break;
+    case "sunriseTheme":
+      return sunriseTheme;
+      break;
+    case "galaxyTheme":
+      return galaxyTheme;
+      break;
   }
 };
 
-export default updateColorScheme;
+const saveThemeToDB = (user, newTheme, setUpdateToFirebasePending) => {
+  const data = {
+    theme: { ...user.theme, currentTheme: newTheme },
+  };
+  const db = firebase.firestore();
+  db.collection("users")
+    .doc(user.uid)
+    .update(data)
+    .catch((error) => {
+      Alert.alert(error);
+    });
+  setUpdateToFirebasePending(true);
+};
